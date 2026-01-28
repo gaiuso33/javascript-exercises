594bdb84c6b2a76fff20eb2d31381c6fce591efb
gaiuso <oluwolegiusayokunle@gmail.com>
2026-01-28 09:24:08 +0100

    solved the exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = str.split("").reverse().join("");
+    return reversed;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
+
