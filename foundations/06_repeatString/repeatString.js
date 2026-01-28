12869c8a31bf3cf4c5815fa0653fe442f836d3cb
gaiuso <oluwolegiusayokunle@gmail.com>
2026-01-28 08:45:05 +0100

    completed this exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -4,7 +4,7 @@ const repeatString = function(string,num) {
     }
     let result= ''
     
-    for(let i=0;i<=num;i++){
+    for(let i=0;i<num;i++){
     result += string
     }
     return result
